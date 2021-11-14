78e5aa682348b36c4a0808c5daf1829f4ff637ee
kalevishal <kalevishal1610@gmail.com>
2021-11-14 13:07:47 +0530

    Added:On save create employee payroll object

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -1,20 +1,12 @@
 class EmployeePayrollData {
     id;
-    get id()
-    {
-        return this._id;
-    }
-
-    set id(id)
-    {this.id=id;
-    }
 
     get name() {
         return this._name;
     }
 
     set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
         if (nameRegex.test(name))
             this._name = name;
         else
@@ -62,17 +54,16 @@ class EmployeePayrollData {
     set startDate(startDate) {
         let currentDate = new Date();
         if (startDate > currentDate) {
-            throw "Start date is a future date";
-        } else {
-            let differnce = Math.abs(currentDate.getTime() - startDate.getTime());
-            let date = differnce / (1000 * 60 * 60 * 24);
-            if (date > 30) {
-                throw "Start date is beyond 30 days";
-            }
+            throw "Start date is a future date"
+        }
+        let difference = Math.abs(currentDate.getTime() - startDate.getTime());
+        let date = difference / (1000 * 60 * 60 * 24);
+        if (date > 30) {
+            throw "Start date is beyond 30 days";
         }
         this._startDate = startDate;
     }
-    
+
     get notes() {
         return this._notes;
     }
@@ -84,7 +75,7 @@ class EmployeePayrollData {
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
-        return "Id = "+ this.id +"Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
     }
 }
 
@@ -100,5 +91,5 @@ function save() {
         }
     }
 
-
+    
 }
